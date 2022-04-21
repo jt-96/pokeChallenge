78831239d0bd6a666c6cccc4e1f799b67ce7386d
jt-96 <jetorrico@outlook.com>
2022-04-21 16:49:56 -0300

    added components for initial mockup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import NavBar from './components/navBar/navBar';
 import SearchBar from './components/searchBar/searchBar';
+import PokeDex from './components/pokeDex/pokeDex';
+import PokeRecent from './components/pokeRecent/pokeRecent';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <SearchBar />
+      <PokeDex />
+      <PokeRecent />
+      <Footer />
     </div>
   );
 }
--- src/components/pokeDex/pokeDex.jsx
+++ src/components/pokeDex/pokeDex.jsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
+import PokeCard from "../pokeCard/pokeCard";
 
 function PokeDex(){
 
     return (
         <Fragment>
             <h1>LOGO - POKEDEX</h1>
+            <PokeCard />
+            <PokeCard />
+            <PokeCard />
         </Fragment>
     )
 }
