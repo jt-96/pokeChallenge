abf449033d6d51e473f194532348742285ab4d50
jt-96 <jetorrico@outlook.com>
2022-04-21 16:48:15 -0300

    added footer component

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function Footer(){
+
+    return (
+        <Fragment>
+            <span>TODOS LOS DERECHOS RESERVADOS</span>
+        </Fragment>  
+    );
+}
+
+export default Footer;
