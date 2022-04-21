b5acf44ed463c37092bc93f10f9b6301be5d60be
jt-96 <jetorrico@outlook.com>
2022-04-21 16:44:19 -0300

    added pokecard component

---
+++ src/components/pokeCard/pokeCard.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+function PokeCard(){
+
+    return (
+        <Fragment>
+            <p>Imagen</p>
+            <p>Número</p>
+            <p>Nombre</p>
+            <span>Tipo 1</span>
+            <span>Tipo 2</span> 
+        </Fragment>
+    );
+}
+
+export default PokeCard;
