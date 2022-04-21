7f7c91e9fa2023f3f8ecf8913f6861bc57a8e8a1
jt-96 <jetorrico@outlook.com>
2022-04-21 18:17:12 -0300

    fetch pokemon for pokecard

--- src/components/pokeCard/pokeCard.jsx
+++ src/components/pokeCard/pokeCard.jsx
@@ -1,14 +1,31 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 
-function PokeCard(){
+function PokeCard(props){
+
+    const [pokemon, setPokemon] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        const getPokemon = async() => {
+            const response = await fetch(props.current.url);
+            const data = await response.json();
+            setPokemon(data);
+            setIsLoaded(true);
+        }
+
+        getPokemon();
+    }, [isLoaded])
 
     return (
         <Fragment>
-            <p>Imagen</p>
-            <p>Número</p>
-            <p>Nombre</p>
-            <span>Tipo 1 </span>
-            <span>Tipo 2</span> 
+            <img src={pokemon?.sprites?.front_default}/>
+            <p>N° {pokemon.order}</p>
+            <p>{pokemon.name}</p>
+            {
+                pokemon?.types?.map((types, index) => (
+                    <span key={index}>{types.type.name} </span>
+                ))
+            }
         </Fragment>
     );
 }
