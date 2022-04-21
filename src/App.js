b101c519bc952390431f570ea368b8309a92e0a4
jt-96 <jetorrico@outlook.com>
2022-04-21 20:51:30 -0300

    moved components used in main app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import NavBar from './components/navBar/navBar';
-import SearchBar from './components/searchBar/searchBar';
 import PokeDex from './components/pokeDex/pokeDex';
-import PokeRecent from './components/pokeRecent/pokeRecent';
 import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <SearchBar />
       <PokeDex />
-      <PokeRecent />
       <Footer />
     </div>
   );
