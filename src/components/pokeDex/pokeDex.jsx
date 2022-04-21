a2ec779cbca1e3dedad8daae30603abeb775a769
jt-96 <jetorrico@outlook.com>
2022-04-21 16:42:57 -0300

    added pokedex component

---
+++ src/components/pokeDex/pokeDex.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function PokeDex(){
+
+    return (
+        <Fragment>
+            <h1>LOGO - POKEDEX</h1>
+        </Fragment>
+    )
+}
+
+export default PokeDex;
