939a1cfa467805fcced1f6c0b01d5f0a9b737970
jt-96 <jetorrico@outlook.com>
2022-04-21 16:46:40 -0300

    added pokeRecent component

---
+++ src/components/pokeRecent/pokeRecent.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function PokeRecent(){
+
+    return (
+        <Fragment>
+            <h1>Logo - RECIENTES</h1>
+        </Fragment>
+    )
+}
+
+export default PokeRecent;
