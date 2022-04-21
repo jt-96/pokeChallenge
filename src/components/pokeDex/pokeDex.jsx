400c65405815d002eaff188537ad7807a1ebfe1e
jt-96 <jetorrico@outlook.com>
2022-04-21 17:29:06 -0300

    added fetch pokemons

--- src/components/pokeDex/pokeDex.jsx
+++ src/components/pokeDex/pokeDex.jsx
@@ -1,14 +1,31 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import PokeCard from "../pokeCard/pokeCard";
 
 function PokeDex(){
 
+    // fetch https://pokeapi.co/api/v2/pokemon?offset=0&limit=9
+    const [currentPokemons, setCurrentPokemons] = useState([]);
+
+    useEffect(() => {
+        const getPokemons = async () => {
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=9');
+            const data = await response.json();
+            setCurrentPokemons(data.results);
+            console.log(data);
+        }
+
+        getPokemons();
+    }, [])
+
+
     return (
         <Fragment>
             <h1>LOGO - POKEDEX</h1>
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
+            {
+                currentPokemons.map((pokemon) => (
+                    <PokeCard current={pokemon}/>
+                ))
+            }
         </Fragment>
     )
 }
