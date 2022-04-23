68047390912fd9ca01ad75b6effb48a973c835d9
jt-96 <jetorrico@outlook.com>
2022-04-23 01:17:20 -0300

    searchbar styling

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,4 +1,5 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import './searchBar.css';
 
 function SearchBar(props) {
 
@@ -10,17 +11,20 @@ function SearchBar(props) {
     }
 
     return (
-        <Fragment>
-            <span>BUSCAR POR NOMBRE</span>
-            <form>
-                <input type="text" name="s" value={searchQuery} onChange={handleInput} />
-            </form>
-
-            <div>
-                <span>Logo</span>
-                <p>BUSCA TU POKEMON POR EL NOMBRE</p>
+        <div className="search">
+            <div className="search__bar">
+                <span className="search__bar__title">BUSCAR POR NOMBRE O NÚMERO</span>
+                <form className="search__bar__form">
+                    <input className="search__bar__input" type="text" name="s" value={searchQuery} onChange={handleInput} />
+                </form>
+                <span className="search__bar__subtitle">Usa la búsqueda avanzada para encontrar Pokemons por otros filtros</span>
             </div>
-        </Fragment>
+
+            <div className="search__info">
+                <img className="search__info__img" src={process.env.PUBLIC_URL + `/img/info.png`} alt="info" />
+                <span>BUSCA TU POKEMON POR EL NOMBRE O USANDO SU NÚMERO DE IDENTIFICACIÓN</span>
+            </div>
+        </div>
     )
 }
 
