b091dd2a5857db3acf6dd34a474988f3bd3fa395
jt-96 <jetorrico@outlook.com>
2022-04-23 10:43:17 -0300

    changed modal & overlay render with portals

--- src/components/pokeModal/pokeModal.jsx
+++ src/components/pokeModal/pokeModal.jsx
@@ -1,41 +1,65 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
+import * as ReactDOM from 'react-dom';
+
+function Overlay() {
+    return <div className="overlay" ></div>
+}
+
+function Modal(props) {
+    return (
+        <div className="modal">
+            <img src={props.selectedPokemon.sprites?.other.home.front_default} alt="Pokemon" />
+            <p>{props.selectedPokemon.name}</p>
+            <p>{props.selectedPokemon.weight}</p>
+            {
+                props.selectedPokemon.types?.map((types, index) => (
+                    <span key={index}>{types.type.name}</span>
+                ))
+            }
+            {
+                props.selectedPokemon.stats?.map((stat, index) => {
+                    return <p key={index}>{stat.stat.name} - {stat.base_stat}</p>
+                })
+            }
+            <button type="button" onClick={props.onCloseModal}>Close</button>
+        </div>
+    );
+}
 
 function PokeModal(props) {
 
     const [selectedPokemon, setSelectedPokemon] = useState({});
 
     useEffect(() => {
-        async function getSelectedPokemon(){
+        async function getSelectedPokemon() {
             const response = await fetch(props.selected.url);
             const data = await response.json();
             setSelectedPokemon(data);
         }
 
         getSelectedPokemon();
-        
+
     }, [props.selected.url])
 
-    function handleModal(){
+    function handleModal() {
         props.onCloseModal(false);
     }
 
     return (
-        <div className="modal">
-            <img src={selectedPokemon.sprites?.other.home.front_default} alt="Pokemon" />
-            <p>{selectedPokemon.name}</p>
-            <p>{selectedPokemon.weight}</p>
+        <Fragment>
             {
-                selectedPokemon.types?.map((types, index) => (
-                    <span key={index}>{types.type.name}</span>
-                ))
+                ReactDOM.createPortal(
+                    <Overlay />,
+                    document.getElementById("overlay")
+                )
             }
             {
-                selectedPokemon.stats?.map((stat, index) => {
-                    return <p key={index}>{stat.stat.name} - {stat.base_stat}</p>
-                })
+                ReactDOM.createPortal(
+                    <Modal selectedPokemon={selectedPokemon} onCloseModal={handleModal} />,
+                    document.getElementById("modal")
+                )
             }
-            <button type="button" onClick={handleModal}>Close</button>
-        </div>
+        </Fragment>
     );
 }
 
